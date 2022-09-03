29881859f02f3af80965ae09bd1e7d7b072f0e50
Jürgen Berchtel <juergen.berchtel@omicronenergy.com>
2022-09-03 13:39:43 +0200

    Add C# project to demonstrate the issue

---
+++ VSSuppressInFileIssue/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+
+namespace VSSuppressInFileIssue
+{
+    // This code is just for demonstrating multiple code analysis warnings.
+    public class Demo
+    {
+        public async Task<string> GetData(Uri apiAddress, string resource)
+        {
+            var httpMessageHandler = new HttpClientHandler { UseDefaultCredentials = true };
+            var client = new HttpClient(httpMessageHandler) { BaseAddress = apiAddress };
+            var result = await client.GetAsync(resource).ConfigureAwait(false);
+            result.EnsureSuccessStatusCode();
+            var response = (await result.Content.ReadFromJsonAsync<ApiResponse>().ConfigureAwait(false)) ?? throw new InvalidOperationException("Empty content");
+            return response.Status;
+        }
+
+        private record ApiResponse(string Status);
+    }
+}
